8deb47e37aa7fc860898e0873ce135ced896a237
Tereza Miklóšová <t.miklosova@seznam.cz>
2025-07-29 11:17:16 +0200

    Flow Hooks, beforeall, beforeeach

--- tests/firstTest.spec.ts
+++ tests/firstTest.spec.ts
@@ -4,13 +4,35 @@ import {test} from '@playwright/test'
 test.beforeEach(async({page}) => {
     // Navigate to the base URL before each test    
     await page.goto('http://localhost:4200/')
-    await page.getByText('Forms').click()
+    
 })
 
-test('first test', async ({page}) => {
-    await page.getByText('Form Layouts').click()
+test.describe.skip('suite1', () => {
+    test.beforeEach(async({page}) => {
+        // Navigate to the base URL before each test    
+        await page.getByText('Forms').click()
+    })
+    
+    test('first test', async ({page}) => {
+        await page.getByText('Form Layouts').click()
+    })
+    
+    test('Navigate to Datepicker page', async ({page}) => {
+        await page.getByText('Datepicker').click()
+    })
 })
 
-test('Navigate to Datepicker page', async ({page}) => {
-    await page.getByText('Datepicker').click()
+test.describe('suite2', () => {
+    test.beforeEach(async({page}) => {
+        // Navigate to the base URL before each test    
+        await page.getByText('Charts').click()
+    })
+    
+    test('Second test', async ({page}) => {
+        await page.getByText('Form Layouts').click()
+    })
+    
+    test('Navigate to Datepicker page1', async ({page}) => {
+        await page.getByText('Datepicker').click()
+    })
 })
